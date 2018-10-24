f77fcf465564c7d34c69c8cc261191b59509d209
Vaidotas Gedzius <vaidotas@gedzius.lt>
2018-10-24 21:51:20 +0100

    Search added

--- common/models/password.js
+++ common/models/password.js
@@ -1,5 +1,32 @@
 'use strict';
 
-module.exports = function(Password) {
+module.exports = (Password) => {
+  /**
+ *
+ * @param {string} s
+ * @param {Function(Error, array)} callback
+ */
 
+  Password.search = (s, callback) => {
+    const filter = {
+      where: {
+        or: [
+          {
+            url: {
+              like: s,
+            }
+          },
+          {
+            userName: {
+              like: s,
+            }
+          }
+        ]
+      }
+    };
+
+    Password.find(filter, (err, passwords) => {
+      callback(null, passwords);
+    });
+  };
 };
